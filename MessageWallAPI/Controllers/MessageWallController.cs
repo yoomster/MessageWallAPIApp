d19aafe14b60ac5a88cb50fa6131db0730f9e820
naomifleurnoel <nperenboom@hotmail.com>
2023-11-07 22:32:31 +0100

    Added Model and Controller

--- MessageWallAPI/Controllers/MessageWallController.cs
+++ MessageWallAPI/Controllers/MessageWallController.cs
@@ -11,12 +11,6 @@ namespace MessageWallAPI.Controllers
             return View();
         }
 
-        // GET: MessageWallController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: MessageWallController/Create
         public ActionResult Create()
         {
@@ -27,48 +21,6 @@ namespace MessageWallAPI.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MessageWallController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: MessageWallController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: MessageWallController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: MessageWallController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
---
+++ MessageWallAPI/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageWallAPI.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
